f86a5ebc50a2246a9a413ad826417c857c217d78
olivia <55671906+ostodieck@users.noreply.github.com>
2023-06-01 19:18:04 +0100

    implement precice-openfoam-comp

--- precice-comp/precice.py
+++ precice-comp/precice.py
@@ -12,10 +12,9 @@ def run_ccx_preCICE(infile: Path, run_folder: Path, participant=None, env=None):
     if not run_folder.is_dir():
         raise ValueError(f"run_folder is not valid folder path: {run_folder}")
 
-    if not isinstance(participant, str):
-        raise ValueError(
-            f"participant is not a valid participant name string: {participant}"
-        )
+    cmd = f"ccx_preCICE -i {infile.stem}"
+    if isinstance(participant, str):
+        cmd += f" -precice-participant {participant}"
 
     if env is isinstance(env, dict):
         for k, v in env:
@@ -23,11 +22,7 @@ def run_ccx_preCICE(infile: Path, run_folder: Path, participant=None, env=None):
             os.environ[k] = str(v)
 
     resp = subprocess.run(
-        "ccx_preCICE"
-        + " -i "
-        + str(infile.stem)
-        + " -precice-participant "
-        + str(participant),
+        cmd,
         cwd=run_folder,
         shell=True,
         check=False,
@@ -35,3 +30,56 @@ def run_ccx_preCICE(infile: Path, run_folder: Path, participant=None, env=None):
         env=os.environ,
     )
     return {"stdout": resp.stdout.decode("ascii"), "returncode": resp.returncode}
+
+
+def run_openfoam_preCICE(
+    run_folder: Path, env=None, tools=Path("/app/precice-openfoam-comp/tools")
+):
+    """Run openfoam adaptor."""
+
+    if not run_folder.is_dir():
+        raise ValueError(f"run_folder is not valid folder path: {run_folder}")
+
+    cmd = "touch fluid-openfoam.foam "
+    cmd += f'&& {str(tools /"run-openfoam.sh")} "$@" '
+    cmd += f'&& source {str(tools /"openfoam-remove-empty-dirs.sh")} && openfoam_remove_empty_dirs'
+
+    if env is isinstance(env, dict):
+        for k, v in env:
+            print(f"Setting environment variable {k} to value {v}")
+            os.environ[k] = str(v)
+
+    resp = subprocess.run(
+        cmd,
+        cwd=run_folder,
+        shell=True,
+        check=False,
+        capture_output=True,
+        env=os.environ,
+    )
+    return {"stdout": resp.stdout.decode("ascii"), "returncode": resp.returncode}
+
+
+def run_openfoam_blockMesh(run_folder: Path, case: str = ""):
+    if not run_folder.is_dir():
+        raise ValueError(f"run_folder is not valid folder path: {run_folder}")
+
+    cmd = "blockMesh"
+    if isinstance(case, str):
+        cmd += f" -case {case}"
+
+    resp = subprocess.run(
+        cmd,
+        cwd=run_folder,
+        shell=True,
+        check=False,
+        capture_output=True,
+        env=os.environ,
+    )
+    return {"stdout": resp.stdout.decode("ascii"), "returncode": resp.returncode}
+
+
+if __name__ == "__main__":
+    run_folder = Path("/home/olivia/tutorials/perpendicular-flap/fluid-openfoam")
+    run_openfoam_blockMesh(run_folder)
+    run_openfoam_preCICE(run_folder, tools=Path("/home/olivia/tutorials/tools"))
