d5a48cbfa22284848ff3fbcbc083ffab97831894
Olivia Stodieck <55671906+ostodieck@users.noreply.github.com>
2023-12-14 07:00:58 +0000

    created and tested .exe via wine on linux

---
+++ wine-comp/helloworld.py
@@ -0,0 +1,15 @@
+import subprocess
+from pathlib import Path
+
+
+def execute_helloworld(run_folder: Path):
+    """Run helloworld windows executatble and save text output to file."""
+
+    resp = subprocess.run(
+        "wine /app/wine-comp/helloworld.exe > helloworld.out",
+        cwd=run_folder,
+        shell=True,
+        check=False,
+        capture_output=True,
+    )
+    return {"stdout": resp.stdout.decode("ascii"), "returncode": resp.returncode}
